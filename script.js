8a2839dadf1ef84994cc792822a5d068a2e6237b
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2021-12-18 11:13:33 +0100

    Modify listeners

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-document.addEventListener("dragstart", () => {});
+let indexBefore;
+let indexDrop;
 
-document.addEventListener("dragover", () => {});
+let drag;
+let itemId;
+let list;
 
-document.addEventListener("drop", () => {});
+document.addEventListener("dragstart", ({ target }) => {
+  itemId = target.id;
+  drag = target;
+  list = target.parentNode.children;
+  for (let i = 0; i < list.length; i += 1) {
+    if (list[i] === drag) {
+      indexBefore = i;
+    }
+  }
+});
+
+document.addEventListener("dragover", (e) => {
+  e.preventDefault();
+});
+
+document.addEventListener("drop", ({ target }) => {
+  if (target.id !== itemId && target.className === "list-item") {
+    drag.remove(drag);
+    for (let i = 0; i < list.length; i += 1) {
+      if (list[i] === target) {
+        indexDrop = i;
+      }
+    }
+    if (indexBefore > indexDrop) {
+      target.before(drag);
+    } else {
+      target.after(drag);
+    }
+  }
+});
