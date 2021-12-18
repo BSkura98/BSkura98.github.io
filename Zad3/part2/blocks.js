444f2ae93f9ddb9b12d3f63f35bfe841c02046c4
Bartłomiej Skura <52361028+BSkura98@users.noreply.github.com>
2021-12-18 12:39:01 +0100

    Add drag&drop for blocks

--- Zad3/part2/blocks.js
+++ Zad3/part2/blocks.js
@@ -1,16 +1,46 @@
-const randomColor = () => {
-  let color = "#";
-  let signs = "0123456789ABCDEF";
-  for (let i = 0; i < 6; i++) {
-    color += signs[Math.floor(Math.random() * 16)];
+function getRandomColor() {
+  var letters = "0123456789ABCDEF";
+  var color = "#";
+  for (var i = 0; i < 6; i++) {
+    color += letters[Math.floor(Math.random() * 16)];
   }
   return color;
+}
+
+var width, height, stage, layer;
+boxlist = [];
+window.onload = () => {
+  width = window.innerWidth * 0.5;
+  height = window.innerHeight;
+
+  stage = new Konva.Stage({
+    container: "canvas",
+    width: width,
+    height: height,
+  });
+
+  layer = new Konva.Layer();
+
+  stage.add(layer);
+
+  var rectX = stage.width();
+  var rectY = stage.height();
+  floor = new Konva.Rect({
+    x: 0,
+    y: rectY - 25,
+    width: rectX,
+    height: 25,
+    fill: "black",
+    draggable: false,
+  });
+  layer.add(floor);
+  stage.add(layer);
 };
 
-const generateBlock = () => {
+function generateBlock() {
   var rectX = stage.width() / 2 - 50;
   var rectY = stage.height() / 2 - 25;
-  var color = randomColor();
+  var color = getRandomColor();
   var box = new Konva.Rect({
     x: rectX,
     y: rectY,
@@ -27,8 +57,14 @@ const generateBlock = () => {
   box.on("mouseout", function () {
     document.body.style.cursor = "default";
   });
-  group.add(box);
-  layer.add(box);
+  box.on("dragend", function () {
+    //tutaj powinno być sprawdzanie kolizji, ale nie wiem sam jak je zrobic
+  });
 
-  layer.draw();
-};
+  layer.add(box);
+  boxlist.push(box);
+  console.log(boxlist);
+  console.log(boxlist[0]);
+
+  stage.add(layer);
+}
